82e0d6ae62c35ae7815e2f22b5613d7e7d49410c
skyfalling <skyfalling@live.com>
2023-07-10 21:47:19 +0800

    init

--- build.sh
+++ build.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env sh
 publish_branch="gitbook"
+publish_source="books"
+publish_dir="docs"
+
 git branch -D "$publish_branch"
 git checkout --orphan  "$publish_branch"
 
-for file in books/*; do
+for file in "$publish_source"/*; do
   gitbook build "$file" docs/"${file##*/}"
 done
 
-for file in $(find docs -name '*.html'); do
+for file in $(find "$publish_dir" -name '*.html'); do
   echo "$file"
   sed -i "" "s|.gitbook/assets/|gitbook/assets/|" "$file"
 done
-for file in docs/*; do
+for file in "$publish_dir"/*; do
   if [ -d "$file/.gitbook/assets" ]; then
     mv "$file/.gitbook/assets" "$file/gitbook/assets"
   fi
 done
+
 git add .
 git commit -m"gitbook build"
 git push origin "$publish_branch"
